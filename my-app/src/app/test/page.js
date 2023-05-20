27c1ca80fc3fbd8717194ba6b7e9028d3d12cf75
jimmy <jimmyhui92012@gmail.com>
2023-05-20 16:50:54 -0700

    added speech to text logic

--- my-app/src/app/test/page.js
+++ my-app/src/app/test/page.js
@@ -1,8 +1,54 @@
-"use client"
-import { Box } from '@chakra-ui/react'
+"use client";
+import { useEffect, useRef, useState } from "react";
 
-export default function Home(){
-    return (
-        <Box m={2}>Tomato</Box>
+
+export default function Home() {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const rec = useRef(new SpeechRecognition())
+    const mic = rec.current;
+    mic.lang = 'en-CAN';
+    mic.continuous = true;
+    // mic.interimResults = false;
+
+    const [micOn, setmicOn] = useState(false);
+    const [answer,setAnswer] =  useState("");
+
+    mic.onresult = (event) => {
+        const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+
+        setAnswer(transcript)
+
+        mic.onerror = (event) => {
+            console.log(event.error);
+        };
+    }
+
+    useEffect(() => {
+        if (micOn) {
+            mic.start();
+            console.log("started")
+        }
+        else{
+            mic.stop();
+            console.log("stopped")
+        }
+        
+    }, [micOn])
+
+
+    return(
+        <div>
+            <button onClick={() => setmicOn(!micOn)}>
+                {micOn ? "Stop" : "Start"}
+            </button>
+
+            <div>
+                {answer}
+            </div>
+        </div>
     )
+
 }
