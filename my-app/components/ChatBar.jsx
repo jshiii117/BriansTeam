c86f458fb8d2392d8f03b68fdc628553f72212a0
kevintan <kevintan@rivian.com>
2023-05-20 15:36:47 -0700

    added basic UI for chat

--- my-app/components/ChatBar.jsx
+++ my-app/components/ChatBar.jsx
@@ -1,31 +1,38 @@
 "use client";
 
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Heading,
-  Stack,
-  StackDivider,
-  Box,
-  Text,
-} from "@chakra-ui/react";
+import ChatBubble from "./ChatBubble";
+import { useState } from "react";
 
 function ChatBar() {
+  const [currentMessage, setCurrentMessage] = useState("");
+
+  const handleSubmit = () => {
+    console.log("button clicked");
+  };
   return (
-    <div class="fixed right-0 bottom-0 w-3/12 h-screen bg-gray-200 flex flex-col">
-      <div class="flex-1 p-4 overflow-y-auto">
-        {/* <!-- Chat messages go here --> */}
-      </div>
-      <div class="p-4 bg-white">
-        <div class="flex">
-          <input
-            type="text"
-            placeholder="Type your message..."
-            class="flex-grow px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <button class="px-4 text-white bg-blue-500 rounded-r-md">Send</button>
+    <div className="w-3/12 bg-gray-200 m-12 rounded-md">
+      <div class="h-screen flex flex-col">
+        <div class="flex-1 overflow-y-auto max-h-[calc(100vh-8rem)]">
+          {/* <!-- Chat messages go here --> */}
+          <ChatBubble />
+          <ChatBubble />
+          <ChatBubble />
+        </div>
+        <div class="bg-white pb-4">
+          <div class="flex">
+            <input
+              type="text"
+              placeholder="Type your message..."
+              class="flex-grow px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <button
+              class="px-4 text-white bg-blue-500 rounded-r-md"
+              onClick={handleSubmit}
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
     </div>
---
+++ my-app/components/ChatBubble.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChatBubble({ props }) {
+  return (
+    <div class="inline-block bg-gray-300 rounded-lg px-4 py-2">
+      <p class="m-0">This is the content of the chat message.</p>
+    </div>
+  );
+}
+
+export default ChatBubble;
