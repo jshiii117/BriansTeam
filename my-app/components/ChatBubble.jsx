e82f5740878f9a4a23d53a593c8109f609b8f649
kevintan <kevintan@rivian.com>
2023-05-20 22:11:07 -0700

    minor changes to styling

--- my-app/components/ChatBubble.jsx
+++ my-app/components/ChatBubble.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ChatBubble({ user, text }) {
   return (
-    <div class="inline-block bg-gray-300 rounded-lg px-4 py-2 m-1 max-w-xs">
+    <div class="inline-block bg-gray-300 rounded-lg px-4 py-2 m-1 flex-wrap">
       <p class="m-0">{text}</p>
     </div>
   );
